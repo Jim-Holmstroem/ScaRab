7f857910c452a574b561ede7de4085ebe69b20bc
kalleakerblom <kalleakerblom@gmail.com>
2015-10-19 17:40:49 +0200

    PEP8 and fixed missing self.

--- Level.py
+++ Level.py
@@ -4,49 +4,43 @@ import numpy as np
 import random
 from rendering import Renderer
 
+
 class Node(object):
-    def __init__(self, color=Renderer.BLACK, coords=(None,None)):
+    def __init__(self, color=Renderer.BLACK, coords=(None, None)):
         self.color = color
         self.openings = {}
         self.walls = {}
         self.coords = coords
 
     def __repr__(self):
-        arrows = {(1,0):"v",(-1,0):"^",(0,1):">",(0,-1):"<"}
-        repr_string="N({})".format("".join([arrows[opening] for opening in self.openings.keys()]))
+        arrows = {(1, 0): "v", (-1, 0): "^", (0, 1): ">", (0, -1): "<"}
+        repr_string = "N({})".format("".join([arrows[opening] for opening in self.openings.keys()]))
         return repr_string
 
-#    def __repr__(self):
-#        if len(self.openings) == 0:
-#            repr_string = 'empty'
-#        else:
-#            vectors = self.openings.keys()
-#            repr_string = str(vectors)
-#        return repr_string
 
 class Level(object):
-    def __init__(self, size=(2,2):
-        self.nodes = np.empty(size,dtype=object)
-        for index,_ in np.ndenumerate(self.nodes):
-            self.nodes[index]=Node(coords=index)
+    def __init__(self, size=(2, 2)):
+        self.nodes = np.empty(size, dtype=object)
+        for index, _ in np.ndenumerate(self.nodes):
+            self.nodes[index] = Node(coords=index)
         self._set_inner_walls()
-        self._make_random_connections(self.nodes[(0,0)])
+        self._make_random_connections(self.nodes[(0, 0)])
     
     def _set_inner_walls(self):
-        for index,node in np.ndenumerate(self.nodes):
-            array_index=np.array(index)
-            for direction in [(1,0),(-1,0),(0,1),(0,-1)]:
-                if all(array_index+direction<=(0,0)) and all(array_index+direction<self.nodes.shape):
+        for index, node in np.ndenumerate(self.nodes):
+            array_index = np.array(index)
+            for direction in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                if all(array_index+direction <= (0, 0)) and all(array_index+direction < self.nodes.shape):
                     neighbor_index = tuple(array_index+direction)
-                    node.walls[direction]=self.nodes[neighbor_index]
+                    node.walls[direction] = self.nodes[neighbor_index]
     
-    def _make_random_connections(self,node):
+    def _make_random_connections(self, node):
         wall_directions = node.walls.keys()
         random.shuffle(wall_directions)
         for direction in wall_directions:
             if not node.walls[direction].openings:
-                make_pairwise_connection(node.coord,node.walls[direction].coord)
-                _make_random_connections(node.neighbors[direction])
+                self._make_pairwise_connection(node.coord, node.walls[direction].coord)
+                self._make_random_connections(node.neighbors[direction])
 
     def _make_connections(self):
         self._make_pairwise_connection(
@@ -76,7 +70,6 @@ class Level(object):
         node2.openings[vector] = node1
         del node1.walls[neg_vector]
         del node2.walls[vector]
-
 
     def display_level(self):
         print(self.nodes)
