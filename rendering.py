d9a47e3768766527bbfeab9ea523fca2f5a150ae
Jim Holmström <jim.holmstroem@gmail.com>
2016-01-17 17:38:10 +0100

    Update rendering.py

--- rendering.py
+++ rendering.py
@@ -16,7 +16,7 @@ class Renderer(object):
     RED = (255, 0, 0)
 
     def __init__(self):
-        size = (700, 600)  # Could be calculated from board size
+        size = (700, 600)
         self.screen = pygame.display.set_mode(size)
         pygame.display.set_caption('ScaRab')
 
@@ -33,7 +33,8 @@ class PlayerViewRenderer(Renderer):
 
 class MapRenderer(Renderer):
     def render(self, level):
-        node_width=40
+        node_width = 40
+	
         self.screen.fill(self.WHITE)
         pygame.draw.lines(
             self.screen,
@@ -47,7 +48,6 @@ class MapRenderer(Renderer):
             10
         )
         for (y, x), node in np.ndenumerate(level.nodes):
-            print(node.walls.keys())
             for direction in node.walls.keys():
                 if direction == (1, 0):
                     start_pos = (x, y+1)
@@ -61,14 +61,13 @@ class MapRenderer(Renderer):
                 elif direction == (0, -1):
                     start_pos = (x, y)
                     end_pos = (x, y+1)
-                start_pix = tuple(40+node_width * np.array(start_pos))
-                end_pix = tuple(40+node_width * np.array(end_pos))
-                print(start_pix,end_pix)
+                start_pixel = tuple(40+node_width * np.array(start_pos))
+                end_pixel = tuple(40+node_width * np.array(end_pos))
                 pygame.draw.line(
                     self.screen,
                     self.BLACK,
-                    start_pix,
-                    end_pix,
+                    start_pixel,
+                    end_pixel,
                     10
                 )
         pygame.display.flip()
